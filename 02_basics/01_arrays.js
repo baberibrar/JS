44d7975b7ef7af4d8bba2758ed923091c10c5c6c
Baber Ibrar <baberibrar@gmail.com>
2024-06-03 17:53:07 +0500

    feat: Add string, number, date, and array examples

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,30 @@
+const myArr = [1, 2, 3, 4, 5];
+console.log([myArr[1]]);
+
+const myHobbies = ['coding', 'reading', 'writing', 'travelling'];
+
+// Array Methods
+
+// 1. push() - Adds an element to the end of an array
+myHobbies.push('cooking');
+console.log(myHobbies);
+
+// 2. pop() - Removes the last element from an array
+myHobbies.pop();
+console.log(myHobbies);
+
+// 3. unshift() - Adds an element to the beginning of an array
+myHobbies.unshift('cooking');
+console.log(myHobbies);
+
+// 4. shift() - Removes the first element from an array
+myHobbies.shift();
+console.log(myHobbies);
+
+// 5. splice() - Adds or removes elements from an array
+myHobbies.splice(1, 1, 'cooking');
+console.log(myHobbies);
+
+// 6. slice() - Returns a portion of an array
+const newHobbies = myHobbies.slice(1, 3);
+console.log(newHobbies);
